fe2180d0e7b52f9551c0e0ae1a395bb5a08056f4
santiago_lucia <santiluciaprivi@gmail.com>
2022-06-02 13:08:55 -0300

    agrego cambios

--- app/src/main/java/com/example/proyecto_final/EstacionarActivity.java
+++ app/src/main/java/com/example/proyecto_final/EstacionarActivity.java
@@ -1,15 +1,44 @@
 package com.example.proyecto_final;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.os.Build;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Switch;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationResult;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.tasks.OnSuccessListener;
 
 public class EstacionarActivity extends AppCompatActivity {
 
+    public static final int DEFAULT_UPDATE_INTERVAL = 30;
+    public static final int FAST_UPDATE_INTERVAL = 5;
+    private static final int PERMISSIONS_FINE_LOCATION = 99;
     TextView tv_lat, tv_lon, tv_altitude, tv_accuracy, tv_speed, tv_sensor, tv_updates, tv_address;
     Switch sw_locationupdates, sw_gps;
+
+    // variable para saber si estoy trackeando
+    boolean updateOn = false;
+
+    // location request es un archivo de configuracion de fused
+    LocationRequest locationRequest;
+
+    LocationCallback locationCallBack;
+
+    // API para sevicios de ubicacion
+    FusedLocationProviderClient fusedLocationProviderClient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,5 +56,143 @@ public class EstacionarActivity extends AppCompatActivity {
         sw_gps = findViewById(R.id.sw_gps);
         sw_locationupdates = findViewById(R.id.sw_locationsupdates);
 
+        locationRequest = LocationRequest.create();
+        locationRequest.setInterval(1000 * DEFAULT_UPDATE_INTERVAL);
+        locationRequest.setFastestInterval(1000 * FAST_UPDATE_INTERVAL);
+        locationRequest.setPriority(LocationRequest.PRIORITY_BALANCED_POWER_ACCURACY);
+
+
+        locationCallBack = new LocationCallback() {
+            @Override
+            public void onLocationResult(@NonNull LocationResult locationResult) {
+                super.onLocationResult(locationResult);
+                // guardar la ubicacion
+                Location location = locationResult.getLastLocation();
+                updateUIValues(location);
+            }
+        };
+
+
+        sw_gps.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                if (sw_gps.isChecked()) {
+                    locationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+                    tv_sensor.setText("Usando sensor GPS");
+                }
+                else {
+                    locationRequest.setPriority(LocationRequest.PRIORITY_BALANCED_POWER_ACCURACY);
+                    tv_sensor.setText("Usando Antenas + WIFI");
+                }
+            }
+        });
+
+        sw_locationupdates.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (sw_locationupdates.isChecked()) {
+                    // enciendo el trackeo
+                    startLocationUpdates();
+                }
+                else {
+                    // apago el trackeo
+                    stopLocationUpdates();
+                }
+            }
+        });
+
+        updateGPS();
+
+    } // fin del metodo onCreate
+
+    private void stopLocationUpdates() {
+        tv_updates.setText("Ubicación detenida.");
+        tv_lat.setText("Sin ubicación");
+        tv_lon.setText("Sin ubicación");
+        tv_speed.setText("Sin ubicación");
+        tv_address.setText("Sin ubicación");
+        tv_accuracy.setText("Sin ubicación");
+        tv_altitude.setText("Sin ubicación");
+        tv_sensor.setText("Sin ubicación");
+
+        fusedLocationProviderClient.removeLocationUpdates(locationCallBack);
+
     }
+
+    private void startLocationUpdates() {
+        tv_updates.setText("Actualizando la ubicación.");
+        fusedLocationProviderClient.requestLocationUpdates(locationRequest, locationCallBack, null);
+        updateGPS();
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        switch (requestCode) {
+            case PERMISSIONS_FINE_LOCATION:
+                if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                   updateGPS();
+                }
+                else {
+                    Toast.makeText(getApplicationContext(), "Esta app requiere permisos para ejecutarse correctamente", Toast.LENGTH_SHORT).show();
+                    finish();
+                }
+            break;
+        }
+
+    }
+
+    private void updateGPS() {
+        // obtener permisos del usuario para trackear ubicacion
+        // obtener la ubicacion actual desde el cliente fused
+        // actualizar la interfaz de usuario
+
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(
+                getApplicationContext()
+        );
+
+        if (ActivityCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.ACCESS_FINE_LOCATION)
+            == PackageManager.PERMISSION_GRANTED) {
+            // el usuario acepto usar el gps
+            fusedLocationProviderClient.getLastLocation().addOnSuccessListener(this, new OnSuccessListener<Location>() {
+                @Override
+                public void onSuccess(Location location) {
+                    // pongo los valores de location en los componetes de la UI
+                    
+                    updateUIValues(location);
+
+                }
+            });
+        }
+        else {
+            // no garantizo los permisos
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                requestPermissions(new String[] {Manifest.permission.ACCESS_FINE_LOCATION}, PERMISSIONS_FINE_LOCATION);
+            }
+        }
+    }
+
+    private void updateUIValues(Location location) {
+        // actualiza todos los text views
+        tv_lat.setText(String.valueOf(location.getLatitude()));
+        tv_lon.setText(String.valueOf(location.getLongitude()));
+        tv_accuracy.setText(String.valueOf(location.getAccuracy()));
+
+        if (location.hasAltitude()) {
+            tv_altitude.setText(String.valueOf(location.getAltitude()));
+        }
+        else {
+            tv_altitude.setText("No disponible");
+        }
+
+        if (location.hasSpeed()) {
+            tv_speed.setText(String.valueOf(location.getSpeed()));
+        }
+        else {
+            tv_speed.setText("No disponible");
+        }
+
+    }
+
 }
